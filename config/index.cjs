1a6226fc2ac7cbc1c86f4774c4baa0ad50390295
Howie <50979483+HErrorz@users.noreply.github.com>
2025-02-27 17:12:16 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -71,7 +71,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: "娜娜",
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "oaExU7MZFaszWucAQZ1-_NZwz6qA",
+      id: "oaExU7FWF-2n5I2uUJ3TcvInzwGs",
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: "ksw00PhrAidq5dCcj5mn9lv8CnWpzSl44fsZW1WBiVE",
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -171,7 +171,7 @@ const USER_CONFIG = {
         "么么～",
         "今天巨巨巨想你",
         "好想老婆啊",
-        "好想你啊臭宝",
+        "好想你啊娜宝",
         "亲晕你",
         "满脑子都是你",
         "啵唧～",
