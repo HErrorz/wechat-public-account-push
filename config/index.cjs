ca668d864541e208c7e66a7cfc62f2ccf3cb3fa9
Howie <50979483+HErrorz@users.noreply.github.com>
2025-02-28 12:40:03 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -73,7 +73,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oaExU7MZFaszWucAQZ1-_NZwz6qA",
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: "5zj4f_tvxQO0eQAdGX7ypaTLF_VOJ2QESmKwJAhqG_Q",
+      useTemplateId: "yDoV1a4AT960GyXitcr041aR5C8ZYdfhjr3H2B8idhQ",
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: "03-08",
       festivals: [
@@ -105,7 +105,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oaExU7MZFaszWucAQZ1-_NZwz6qA",
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: "5zj4f_tvxQO0eQAdGX7ypaTLF_VOJ2QESmKwJAhqG_Q",
+      useTemplateId: "yDoV1a4AT960GyXitcr041aR5C8ZYdfhjr3H2B8idhQ",
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: "03-08",
       festivals: [
